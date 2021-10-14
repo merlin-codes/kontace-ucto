391373884e81303ed7e6e1cf3b9c7d6bfa9bf973
MiloslavStekrt <stekrt18@gmail.com>
2021-10-14 16:46:03 +0200

    fix not working refresh token

--- server/fuckit/useless.ts
+++ server/fuckit/useless.ts
@@ -1,13 +1,14 @@
 import { Credentials, OAuth2Client } from "google-auth-library";
-import { GoogleAuth } from "googleapis-common";
 
 export function refreshIt(client: OAuth2Client, token: Credentials): Promise<Credentials> {
     return new Promise((r, rej) => {
-        client.refreshAccessToken((e, token) => {
-            if (e) console.log(e);
-            console.log(`token is: ${token}`);
-            
-            r(token!);
-        })
+        if (token.refresh_token) {
+            client.refreshAccessToken((e, token) => {
+                if (e) console.log(e);
+                console.log(`token is: ${token}`);
+                
+                r(token!);
+            })
+        }
     })
 }
